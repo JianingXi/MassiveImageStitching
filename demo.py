900250cfb243412dc5feb8b56f0ea01134d63160
Jianing Xi <xjn@gzhmu.edu.cn>
2025-04-09 14:58:20 +0800

    minor
    minor

--- demo.py
+++ demo.py
@@ -3,9 +3,9 @@ from cut_white_margin_img import trim_white_border
 
 
 
-input_dir = r'C:\Users\xijia\Desktop\D20240729_海南比赛新闻稿\比赛排名截图'
-mid_pic = r'C:\Users\xijia\Desktop\D20240729_海南比赛新闻稿\output_image.jpg'
-output_pic = r'C:\Users\xijia\Desktop\D20240729_海南比赛新闻稿\output_image.jpg'
+input_dir = r'C:\Users\xijia\Downloads\思政案例\test4'
+mid_pic = r'C:\Users\xijia\Downloads\思政案例\test444.png'
+output_pic = mid_pic
 
 
 create_image_grid(input_dir, mid_pic, padding=10)
